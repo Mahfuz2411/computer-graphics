eaa6122cd2f88964102e5e5bfd357bcd85818113
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2024-10-30 02:10:22 +0600

    first commit

---
+++ DDA/dda.py
@@ -0,0 +1,24 @@
+x1, y1 = list(map(int, input("First point: ").split()))[:2]
+x2, y2 = list(map(int, input("Second point: ").split()))[:2]
+
+dx = abs(x1 - x2)
+dy = abs(y1 - y2)
+m = dy/dx
+steps = dx if m<=1 else dy
+
+xinc = round(dx/steps, 2)
+yinc = round(dy/steps, 2)
+
+data = []
+data.append([x1, y1])
+
+x, y = x1, y1
+for _ in range(steps):
+    x += xinc
+    y += yinc
+    x = round(x, 2)
+    y = round(y, 2)
+    data.append([x, y])
+
+for arr in data:
+    print(arr)
