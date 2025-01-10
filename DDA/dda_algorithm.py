1dcc9f1fa1206f101dc4659df5329cf54173f1aa
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2025-01-11 04:20:39 +0600

    bug fixed on dda algorithm

--- DDA/dda_algorithm.py
+++ DDA/dda_algorithm.py
@@ -1,12 +1,12 @@
 import math as mt
 
 class DDA:
-    def __init__(self, start: list, end: list):
+    def __init__(self, start: list, end: list) -> None:
         self.start: list = start
         self.end: list = end
         self.points: list = []
     
-    def findPoints(self):
+    def findPoints(self) -> list[list[int, int]]:
         dx = self.end[0] - self.start[0]
         dy = self.end[1] - self.start[1]
         
@@ -23,19 +23,23 @@ class DDA:
         y: int
         
         x, y = self.start
-        while x < self.end[0] or y < self.end[1]:
+        while 1:
             x += xinc
             y += yinc
             
+            if x>self.end[0] or y>self.end[1]: break
             self.points.append([round(x), round(y)])
+        
+        return self.points
 
 
 def main():
     start = list(map(int, input("Start: ").split()))[:2]
     end = list(map(int, input("End: ").split()))[:2]
-    ob = DDA(start, end)
-    ob.findPoints()
-    print(ob.points)
+    # ob = DDA(start, end)
+    # ob.findPoints()
+    # print(ob.points)
+    print(DDA(start, end).findPoints())
     
 
 if __name__ == "__main__":
